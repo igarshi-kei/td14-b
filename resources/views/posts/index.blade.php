1f77f5f3f154258fea4989152d6416ffc5f45db8
akashiakashi <akashishukatsu@gmail.com>
2023-12-10 17:45:32 +0900

    装飾おわり！

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,7 +15,7 @@
         <h2>✨投稿一覧画面✨</h2>
         <p class="text-lg">みんなの【ききたいこと】や【つたえたいこと】をサンタさんにおくると、おへんじがもらえるよ❕</p>
         <div class="mt-2 mb-2">
-            <a href='/posts/create' class="bg-gradient-to-br from-gray-300 to-gray-800 hover:bg-gradient-to-tl text-white rounded px-4 py-2">新規投稿</a>
+            <a href='/posts/create' class="bg-green-600 hover:bg-green-500 text-white rounded px-4 py-2"">新規投稿</a>
         </div>
         <div>
             @foreach ($posts as $post)
