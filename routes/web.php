688fca30975441951cc445a7a20a284c907f848e
akashiakashi <akashishukatsu@gmail.com>
2023-12-25 16:54:15 +0900

    コミット練習

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,11 @@ use App\Http\Controllers\CategoryController;
 */
 
 
-    Route::get('/', function () { return view('welcome'); });
+
+
+    Route::get('/', function () { 
+        return view('welcome');
+    });
 
     Route::get('/dashboard', function () {
     return view('dashboard');
